b4d3aea7f50e453df5655b067b52ba1c53efabf9
emerson <manoemmet3@gmail.com>
2024-06-21 21:06:11 -0300

    se liga menor

--- main/urls.py
+++ main/urls.py
@@ -5,5 +5,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.landing_page, name='landing_page'),
-    
+    path('login/', views.login_view, name='login'),    
 ]
--- main/views.py
+++ main/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-
+from django.shortcuts import render
 
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+
+def login_view(request):
+    return render(request, 'login.html')
